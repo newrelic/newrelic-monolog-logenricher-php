0e7f29a852958a8dcf6b6fe5ad0bb14646a3cede
Adam Harvey <adam@adamharvey.name>
2019-12-04 14:31:52 -0800

    Conditionally define classes, not just includes.
    This is required to work around an issue when Composer optimises the
    autoloader into a static map: it will use the first Handler it finds,
    rather than the one that's correct for the given Monolog version.
    
    Eventually, we'll drop support for Monolog 1, and we can get rid of all
    this hackery.

--- src/api1/Handler.php
+++ src/api1/Handler.php
@@ -22,65 +22,67 @@ use Monolog\Handler\HandlerInterface;
 use Monolog\Logger;
 use Monolog\Util;
 
-class Handler extends AbstractHandler
-{
-    /**
-     * Delegates upload of single record to send()
-     *
-     * @param array $record
-     */
-    protected function write(array $record)
+if (Logger::API == 1) {
+    class Handler extends AbstractHandler
     {
-        $this->send($record["formatted"]);
-    }
-
-    /**
-     * Iterates over batched data, filtering out logs with levels
-     * lower than the constructed threshold. If applicable logs are found,
-     * they are formatted as a JSON array compatible with New Relic's batch
-     * Log ingest and delegated to sendBatch()
-     *
-     * @param array $record
-     */
-    public function handleBatch(array $records)
-    {
-        $level = $this->level;
-        $records = array_filter($records, function ($record) use ($level) {
-            return ($record['level'] >= $level);
-        });
-        if ($records) {
-            $this->sendBatch($this->getFormatter()->formatBatch($records));
+        /**
+         * Delegates upload of single record to send()
+         *
+         * @param array $record
+         */
+        protected function write(array $record)
+        {
+            $this->send($record["formatted"]);
         }
-    }
 
-
-    /**
-     * Sets Handler's Formatter. Note: only NewRelic\Monolog\Enricher\Formatter
-     * is compatible.
-     *
-     * @param FormatterInterface $formatter
-     *
-     * @throws InvalidArgumentException If incompatible Formatter given
-     */
-    public function setFormatter(FormatterInterface $formatter)
-    {
-        if ($formatter instanceof Formatter) {
-            return parent::setFormatter($formatter);
+        /**
+         * Iterates over batched data, filtering out logs with levels lower
+         * than the constructed threshold. If applicable logs are found, they
+         * are formatted as a JSON array compatible with New Relic's batch Log
+         * ingest and delegated to sendBatch()
+         *
+         * @param array $record
+         */
+        public function handleBatch(array $records)
+        {
+            $level = $this->level;
+            $records = array_filter($records, function ($record) use ($level) {
+                return ($record['level'] >= $level);
+            });
+            if ($records) {
+                $this->sendBatch($this->getFormatter()->formatBatch($records));
+            }
         }
-        throw new \InvalidArgumentException(
-            'NewRelic\Monolog\Enricher\Handler is only compatible with '
-            . 'NewRelic\Monolog\Enricher\Formatter'
-        );
-    }
+
+
+        /**
+         * Sets Handler's Formatter. Note: only
+         * NewRelic\Monolog\Enricher\Formatter is compatible.
+         *
+         * @param FormatterInterface $formatter
+         *
+         * @throws InvalidArgumentException If incompatible Formatter given
+         */
+        public function setFormatter(FormatterInterface $formatter)
+        {
+            if ($formatter instanceof Formatter) {
+                return parent::setFormatter($formatter);
+            }
+            throw new \InvalidArgumentException(
+                'NewRelic\Monolog\Enricher\Handler is only compatible with '
+                . 'NewRelic\Monolog\Enricher\Formatter'
+            );
+        }
 
     
-    /**
-     * Returns a New Relic Formatter initialized with API compatible values
-     *
-     * @return Formatter
-     */
-    protected function getDefaultFormatter()
-    {
-        return new Formatter(Formatter::BATCH_MODE_JSON, false);
+        /**
+         * Returns a New Relic Formatter initialized with API compatible values
+         *
+         * @return Formatter
+         */
+        protected function getDefaultFormatter()
+        {
+            return new Formatter(Formatter::BATCH_MODE_JSON, false);
+        }
     }
 }
